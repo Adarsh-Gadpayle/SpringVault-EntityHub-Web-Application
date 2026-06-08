22f983dc3a32d1935e291ab249e605b27cbbed8f
Adarsh Ashok Gadpayle <160723195+Adarsh-Gadpayle@users.noreply.github.com>
2026-06-08 18:08:18 +0530

    Add files via upload

---
+++ MyMiniProject1Application.java
@@ -0,0 +1,13 @@
+package in.soft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMiniProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMiniProject1Application.class, args);
+	}
+
+}
